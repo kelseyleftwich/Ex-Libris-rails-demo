93c565bb9a28348731d8d1ed5387a9da4bc307ca
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-04-16 14:43:55 -0500

    Adds create method to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,14 @@
 class BooksController < ApplicationController
+  def create
+    Book.create(book_params)
+
+    redirect_to root_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
+  end
+
 end
