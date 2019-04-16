698b3d8a632d2104d2cb3a067704192a4c0bd38a
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-04-16 15:25:53 -0500

    Adds note create and book-note relationship

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,15 @@
 class NotesController < ApplicationController
+  def create
+    book = Book.find(params[:note][:book_id])
+
+    book.notes.create(note_params)
+
+    redirect_to book
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:page_number, :body)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+
+  has_many :notes
   def thumbnail
     "https://via.placeholder.com/64"
   end
